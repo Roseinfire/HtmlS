27a33b4fa49ee0a19de21c34ef5fabeab10923bb
Roseinfire <14box14@gmail.com>
2023-01-02 16:10:01 +0300

    Update begin.js

--- begin.js
+++ begin.js
@@ -2,7 +2,7 @@ fetch("https://roseinfire.github.io/HtmlScript/document.json")
   .then(response => response.text())
   .then(text => estableHtmlScript(text))
   
-  function estabeHtmlScript(text) {
+  function estableHtmlScript(text) {
     window.__manual__ = (function() {
        var scripts = document.getElementsByTagName("script")
        for(var i = 0; i < scripts.length; i++) {
