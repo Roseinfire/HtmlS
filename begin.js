797b207d6610972a2ccf303c7dd63046227d51a4
Roseinfire <14box14@gmail.com>
2023-01-06 13:15:11 +0300

    Add files via upload

--- begin.js
+++ begin.js
@@ -1,4 +1,4 @@
-function getscript(source1, source2) {
+function getmain(source1, source2) {
   var scripts = document.getElementsByTagName("script")
     for(var i = 0; i < scripts.length; i++) {
       if(scripts[i].src == source1 || scripts[i].src == source2) { return scripts[i] }
@@ -6,21 +6,20 @@ function getscript(source1, source2) {
   var scr = document.createElement("script")
   return scr
  };
-window.getouter =  function(name) { 
-   var list = __manual__.attributes
+window.getouter =  function(name, from) { 
+   var list = from.attributes
    for(var i = 0; i < list.length; i++) { if(list[i].name == name) { return list[i].value } } 
    return undefined
  };
  window.addEventListener("load", function() {
-   window.__manual__ = document.body.parentElement
-   var host = getouter("localhost")
+   var host = getouter("localhost", document.body.parentElement)
    if(host == undefined) {
-     __manual__ = getscript("https://roseinfire.github.io/HtmlScript/begin.js")
+     __htmlscript__ = getmain("https://roseinfire.github.io/HtmlScript/begin.js")
      fetch("https://roseinfire.github.io/HtmlScript/document.json")
        .then(response => response.text())
        .then(text => estable(text))
    } else {
-    __manual__ = getscript("http://localhost:" + host + "/begin.js","https://roseinfire.github.io/HtmlScript/begin.js")
+    __htmlscript__ = getmain("http://localhost:" + host + "/begin.js","https://roseinfire.github.io/HtmlScript/begin.js")
     fetch("document.json")
       .then(response => response.text())
       .then(text => estable(text))
@@ -28,5 +27,14 @@ window.getouter =  function(name) {
  });
 function estable(response) {
    console.group("compilation")
-   console.log(__manual__); document.write(response)
+   window.__head__ = document.head
+   window.__scripts__ = (function() {
+      var res = []
+      var scripts = document.getElementsByTagName("script")
+      for(var i = 0; i < scripts.length; i++) {
+        var type  = getouter("type", scripts[i])
+        if( type == "htmlscript" || type == "text/htmlscript") { res.push(scripts[i]) }
+          }; return res
+      })(); console.log(__scripts__)
+   document.write(response)
 }
