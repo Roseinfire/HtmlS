acd7837f6253cc53d596afdd434ce9c39a5dcee5
Roseinfire <14box14@gmail.com>
2023-01-08 12:32:56 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 <html>
    <head>` + `
        <meta charset="UTF-8">` + `
-       <scr` + `ipt src="https://roseinfire.github.io/HtmlScript/begin.js">
-    
-       </scr`+"ipt>" +`
+       <script type="htmlscript"></scr` + `ipt>` + `
+       <script src="https://roseinfire.github.io/HtmlScript/begin.js"></scr`+"ipt>" +`
     </head>` + `
 </ht`+"ml>"
           navigator.clipboard.writeText(form);
-          document.getElementById("copy").innerHTML = "successfull"
+          document.getElementById("copy").innerHTML = "source coppied"
        }
      
