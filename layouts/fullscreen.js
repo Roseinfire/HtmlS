498d7daa05477c5abf112899a82d867d7c31ef3a
Roseinfire <14box14@gmail.com>
2023-01-11 19:49:43 +0300

    Add files via upload

--- layouts/fullscreen.js
+++ layouts/fullscreen.js
@@ -1,6 +1,8 @@
   /* layout = "fullscreen" */
 new Layout(
   function(e, arg) {
-     e.style.width = innerWidth + "px"
-     e.style.height = innerHeight + "px"
+      hand.style.paddingBottom = "10px"
+      hand.style.paddingTop = "10px"
+     e.style.width = innerWidth+ "px"
+     e.style.height = innerHeight+ "px"
    }, "fullscreen")
