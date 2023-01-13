d0d827b036bf6a7fcecc98ac450e93846839ba6b
Roseinfire <14box14@gmail.com>
2023-01-13 17:24:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function join() {
              height: ${innerHeight*0.7}px; margin-left: ${innerWidth*0.15}px; margin-top: ${innerHeight*0.15}px`
           frame.src = "https://embed.lottiefiles.com/animation/132607"
           frame.onload = function() {
-             setTimeout(function() { document.body.removeChild(frame) }, 900)
+             // setTimeout(function() { document.body.removeChild(frame) }, 900)
            }; document.body.append(frame)
       }
      
