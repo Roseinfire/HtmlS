aab824cbc56c4fa8288cb1c92c483e01acbffa0f
Roseinfire <14box14@gmail.com>
2023-01-13 17:31:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function join() {
           document.getElementById("copy").innerHTML = "Welcome"
           /* Thanks Kalman Huang at https://lottiefiles.com */
           var frame = document.createElement("iframe")
-          frame.style = `potition: fixed; width: ${innerWidth*0.7}px;
-             height: ${innerHeight*0.7}px; margin-left: ${innerWidth*0.15}px; margin-top: ${innerHeight*0.15}px`
+          frame.style = `potition: fixed; width: 90%;
+             height: 90%; margin-left: 5; margin-top: 5%; border: 0;`
           frame.src = "https://embed.lottiefiles.com/animation/132607"
           frame.onload = function() {
-             // setTimeout(function() { document.body.removeChild(frame) }, 900)
+             setTimeout(function() { document.body.removeChild(frame) }, 3000)
            }; document.body.append(frame)
       }
      
