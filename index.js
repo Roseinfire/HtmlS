5e4e2aeccfaa0c0c82734d4c06446ee030f8084e
Roseinfire <14box14@gmail.com>
2023-01-14 08:20:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
  frame.src = "https://embed.lottiefiles.com/animation/132607"
  document.body.append(frame)
  function join() {
+          frame.onload = function() { 
+            frame.style.display = "block" 
+            setTimeout(function() { document.body.removeChild(frame) }, 10000)
+          }
           frame.src += ""
-          frame.style.display = "block"
           document.getElementById("copy").innerHTML = "Welcome"
-          setTimeout(function() { document.body.removeChild(frame) }, 10000)
       }
      
