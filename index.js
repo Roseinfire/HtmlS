d6d1b2be971d7ecce063f77e75e061054e7f63cc
Roseinfire <14box14@gmail.com>
2023-01-14 07:56:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-fetch("https://embed.lottiefiles.com/animation/132607")
-function join() {
-       var form = `
-<html>
-   <head>` + `
-       <meta charset="UTF-8">` + `
-       <script type="htmlscript"></scr` + `ipt>` + `
-       <script src="https://roseinfire.github.io/HtmlScript/begin.js"></scr`+"ipt>" +`
-    </head>` + `
-</ht`+"ml>"
-         // navigator.clipboard.writeText(form);
+/* Thanks Kalman Huang at https://lottiefiles.com */
+ var frame = document.createElement("iframe")
+ frame.style = `position: fixed; width: 90%; top: 0;
+ height: 90%; margin-left: 5%; margin-top: 5%; border: 0; display: none;`
+ frame.src = "https://embed.lottiefiles.com/animation/132607"
+ document.body.append(frame)
+ function join() {
+          frame.style.display = "block"
           document.getElementById("copy").innerHTML = "Welcome"
-          /* Thanks Kalman Huang at https://lottiefiles.com */
-          var frame = document.createElement("iframe")
-          frame.style = `position: fixed; width: 90%; top: 0;
-             height: 90%; margin-left: 5%; margin-top: 5%; border: 0;`
-          frame.src = "https://embed.lottiefiles.com/animation/132607"
-          frame.onload = function() {
-           setTimeout(function() { document.body.removeChild(frame) }, 10000)
-           }; document.body.append(frame)
+          frame.contentDocument.location.reload(true);
+          setTimeout(function() { document.body.removeChild(frame) }, 10000)
       }
      
