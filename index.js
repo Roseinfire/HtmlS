bf38d79bb6d43cf10cd7c37df808c0d75246cf9c
Roseinfire <14box14@gmail.com>
2023-01-28 15:20:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* Thanks Kalman Huang at https://lottiefiles.com */
  var frame = document.createElement("iframe")
  frame.style.display = "none"
- onResize(frame, function(fr) {
+ onResize([frame], function(fr) {
     e = fr.style
     e.position = "fixed"
-    e.width = `{innerWidth}px`
-    e.height = `{innerHeight}px`
+    e.width = `${innerWidth}px`
+    e.height = `${innerHeight}px`
     e.top = 0; e.border = 0;
- });
+ }); __resize()
  frame.src = "https://embed.lottiefiles.com/animation/132607"
  document.body.append(frame)
  function join() {
