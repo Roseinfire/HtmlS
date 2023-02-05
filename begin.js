ca4442a57a0aeb346a7d33dee4cf57f095dc79e6
Roseinfire <14box14@gmail.com>
2023-02-05 16:38:22 +0300

    Update begin.js

--- begin.js
+++ begin.js
@@ -1,57 +1,144 @@
-window.getouter =  function(name, from, onfail) { 
-   var list = from.attributes
-   for(var i = 0; i < list.length; i++) { if(list[i].name == name) { return list[i].value } } 
-   if(onfail) { return onfail }
-   return undefined
- };
-function loadtheme() {
-    window.__loading__ = document.createElement("div")
-    var ld = __loading__
-    ld.style.width = innerWidth + "px"
-    ld.style.position = "fixed"
-    ld.style.fontSize = "35px"
-    ld.style.left = 0; ld.style.top = 0;
-    ld.style.textAlign = "center"
-    ld.innerHTML = "Loading.."
-    ld.style.color = getouter("theme", document.body, "rgba(217, 210, 210, 0.6)")
-    if(document.body) {
-     document.body.style.margin = 0;
-     document.body.append(ld)
-    }
-     ld.top = function() { ld.style.marginTop = (innerHeight-ld.offsetHeight)/2 + "px"}
-     ld.top()
-  };
-function getmain(source1, source2) {
-  var scripts = document.getElementsByTagName("script")
-    for(var i = 0; i < scripts.length; i++) {
-      if(scripts[i].src == source1 || scripts[i].src == source2) { return scripts[i] }
-    }
-  var scr = document.createElement("script")
-  return scr
- };
- window.addEventListener("load", function begin() {
-   loadtheme()
-   var host = getouter("host", document.body.parentElement, "https://roseinfire.github.io/HtmlScript")
-     fetch(host + "/document.json")
-       .then(response => response.text())
-       .then(text => estable(text))
-        .catch((error) => {
-         __loading__.innerHTML = "Host Error :("
-         console.error('Error:', error);
-         console.log("https://github.com/Roseinfire/HtmlScript for more information.")
-        })
- });
-function estable(response) {
-   console.group("compilation")
-   window.__head__ = document.head
-   window.__body__ = document.body
-   window.__scripts__ = (function() {
-      var res = []
-      var scripts = document.getElementsByTagName("script")
-      for(var i = 0; i < scripts.length; i++) {
-        var type  = getouter("type", scripts[i])
-        if( type == "htmlscript" || type == "text/htmlscript") { res.push(scripts[i]) }
-          }; return res
-      })(); console.log(__scripts__)
-  document.write(response)
-}
+/* This working script. It loads form of the future document
+  and creates basic functions and constants. 
+  Do not use it without document.json and network connection.
+  Licensed under MIT, Roseinfire, 2023. */
+  
+  function onlyNumbers(text="") { 
+      var res = ""
+      for(var i = 0; i < text.length; i++) {
+          for(var e = 0; e <= 9; e++) {
+              if(text[i] == e.toString()) { res += text[i]; break }
+              } 
+          } 
+      if(res) { return eval(res) } else { return 0 } 
+      };
+  
+  function getouter(name, from=document.body, onfail="") {
+      var list = from.attributes
+      for(var i = 0; i < list.length; i++) { 
+          if(list[i].name == name) { return list[i].value }
+          }
+      if(onfail) { return onfail }
+      return null
+      };
+  
+  function loadtheme() {
+      __loading__ = document.createElement("div")
+      }; loadtheme()
+  
+  let fetches = new Array()
+  class ExtendFetch {
+      constructor(src, chain=function() {}, onerror=function() {}) {
+          const that = this
+          this.source = src
+          this.chain = chain
+          this.load = function(chainHandler=function(ch, t) { return ch(t) }) {
+              fetch(that.source)
+              .then(response => response.text())
+              .then(text => chainHandler(chain, text))
+              .catch(error => onerror(error))
+              }
+          }
+      };
+  
+  function syncFetch(extendFetchList=[]) {
+      var list = extendFetchList
+      var chains = new Array()
+      var status = list.length
+      function loadstatus(i) {
+          status--
+          if(!status) { return true }
+          }
+      for(var i = 0; i < list.length; i++) {  
+          (function (index) {
+              list[i].load(function(ch, t) {
+                  chains.push({ item: ch, index: index, text: t });
+                  if( loadstatus(index) ) {  exeChains() }
+                  })
+              })(i)
+          }
+      function exeChains() {
+          var order = chains.sort(function(a, b) { if(a.index > b.index) { return 1 } else { return -1 } })
+          for(var i = 0; i < order.length; i++) { 
+              var pointer = (order[i+1]) ? (order[i+1].item) : ( null )
+              order[i].item(order[i].text, pointer)
+              }
+          }
+      }
+  
+  __host__ = getouter("host", document.body.parentElement, "https://roseinfire.github.io/HtmlS")
+
+  fetches.push(new ExtendFetch(__host__ + "/document.json", estable, function(err) { 
+      /* __loading__ = "Host Error :(" */ 
+   }))
+
+  fetches.push(new ExtendFetch(__host__ +  "/layouts/" + __layout__.name + ".js", setlayout,  function() {}))
+
+  __data__ = ""
+  for(var i = 0; i < __scrips__.length; i++) {
+    var source = getouter("fetch", __scripts__[i], null)
+      if(source) {
+       fetches.push(new ExtendFetch(__host__ +  source, function(res, pointer) {
+             __data__ += res; if(!pointer) { document.write(response) }
+           }, function() {}))
+         }
+      }
+   }; syncFetch(fetches)
+  
+  function estable() {
+      console.group("compilation")
+      window.__head__ = document.head
+      window.__body__ = document.body
+      window.__layout__ = searchlayouts("relative 0.6")
+      var title = document.title
+      if(title) { window.__title__ = title } 
+      else {  window.__title__ = "Untitled" }
+      window.__scripts__ = (function() {
+          var res = []
+          var scripts = document.getElementsByTagName("script")
+          for(var i = 0; i < scripts.length; i++) {
+              var type  = getouter("type", scripts[i])
+              if( type == "htmlscript" || type == "text/htmls") { res.push(scripts[i]) }
+              }; return res
+          })() // console.log(__scripts__)
+      };
+
+  var resizebase = new Array()
+  function onResize(e, f) { 
+      resizebase.push({ func: f, elem: e }); return resizebase
+      }
+  
+  class Layout {
+      constructor(reaction=function() {}, name) {
+          this.content = reaction
+          this.name = name
+          }
+      };
+  
+  function searchlayouts(onfail) {
+      var layout = getouter("layout", document.head, onfail)
+      return (function layout(val) {
+          var action = false
+          var pre = true
+          var res = ""
+          for(var i = 0; i < val.length; i++) {
+              if(val[i] != " " && pre) { pre = false }
+              if(val[i] != " " && !pre) { res += val[i] }
+              if(val[i] == " " && !pre) { action = i; break }
+              }
+          var argument = "";
+          if(action) {
+              var pre = true
+              for(var i = action+1; i < val.length; i++) {
+                  if(val[i] != " " && pre) { pre = false }
+                  if(val[i] != " " && !pre) { argument += value[i] }
+                  else if(val[i] == " " && !pre) { return argument }
+                  }
+              }; return { name: res, arg: argument }
+          })(layout)
+      };
+  
+  function setlayout(response) {
+        console.log(response)
+    //  onResize([hand], function(e) { layout.content(e, searchplans.plan.argument) })
+   };
