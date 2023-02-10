0a5a6391630a02af9359ac87f2325a7d22f56359
Roseinfire <14box14@gmail.com>
2023-02-10 11:46:13 +0300

    Update Contribution.js

--- Contribution.js
+++ Contribution.js
@@ -4,7 +4,7 @@
     makes pointers for files and tips for future contributors.
     HtmlS was created to finally solve all the problems of html.
     HtmlS is open source. Anyone can take part in this project.
-        How does all it work?
+      How does all it work?
           Project divided into three main parts: 
             1) Mathematical ("begin.js" file)
             2) Theoretical ("iterations.js" file)
@@ -20,16 +20,16 @@
             This part is about features, sugar and JS generated html. May require some imagination and a lot of knowledge about html.
          While the first two parts are both theoretical, the third is more about connecting two others and especially testing results.
             Not only original ideas, but tests and much more tests is what we really need. That's why we believe in open source.
-    Before you begin..
+      Before you begin..
            Making your contribution might be easier than you think!
            You can just write an addition - file which doesn't need a lot of knowledge, but is very important for further development.
            Creating additions is a good way to support us, which is carefully described in project Wiki.
-    And, whether you decided to work with language itself..
+      And, whether you decided to work with language itself..
            All the files contain notes for functions and solutions, every solution divided from others.
            Engage the community and share commentaries for your work.
            To avoid different tabulation standards - use JStab (github.com/Roseinfire/JStab)
             Program will carefully set up spaces and separate functions.
            The best way to communicate with us - github, but we don't mind whether you send an email.
-    And of course, Happy Coding!
+      And of course, Happy Coding!
 */
 
