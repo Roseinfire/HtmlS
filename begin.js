02e78b0997f584431f45069a9a40c9e4a66c57f6
Roseinfire <14box14@gmail.com>
2023-03-03 13:31:33 +0300

    Update begin.js

--- begin.js
+++ begin.js
@@ -159,12 +159,12 @@
         script.innerHTML = __iterations__
         document.body.append(script)
         console.group("compilation")
-        createDocument(__metadata__)
         };
 
    function setlayout(response) { // create layout 
         __layout__.js = new Layout(null, "default") // estable default value
         try { __layout__.js = eval(response) } catch { console.warn(`unexpected layout -->`, response) } // try to estable loaded layout
+        createDocument(__metadata__) // important to call when htmls package is loaded
         };
 
    /* LOADING */
